2b01e054b11859bfd2158faecd3d797f298217dc
Pulkit Pahuja <pulkit.pahuja98@gmail.com>
2021-05-08 17:08:12 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ app.post('/tweets', (req, res) => {
 
 var options = {
     headers: {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
     }
 }
 
@@ -211,7 +211,7 @@ cron.schedule('0 0 */1 * * *', () => {
         for (var user of result) {
             const { pinCode, ageGroup, email } = user;
             request.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pinCode}&date=${moment().format("DD-MM-yyyy")}`
-                , function (error, response, body) {
+                , options, function (error, response, body) {
                     const data = createData(JSON.parse(body)["centers"], ageGroup[0])
                     if (data.length > 0) {
                         sendEmail(data, email, pinCode, ageGroup).then(stat => {
@@ -231,8 +231,7 @@ app.post('/vaccinated', (req, res) => {
     }
     if (type == "now") {
         request.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pinCode}&date=${moment().format("DD-MM-yyyy")}`
-            , function (error, response, body) {
-                console.log(body, response)
+            , options, function (error, response, body) {
                 const data = createData(JSON.parse(body)["centers"], ageGroup)
                 if (data.length > 0) {
                     sendEmail(data, email, pinCode, ageGroup).then(stat => {
